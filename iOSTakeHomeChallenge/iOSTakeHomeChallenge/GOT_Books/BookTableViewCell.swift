775e8d4d751f6d76ba331c1bec97056fe703d6f2
Rastislav Smolen <>
2021-09-13 12:54:26 +0100

    Created formatted date and displayed correct format

--- iOSTakeHomeChallenge/iOSTakeHomeChallenge/GOT_Books/BookTableViewCell.swift
+++ iOSTakeHomeChallenge/iOSTakeHomeChallenge/GOT_Books/BookTableViewCell.swift
@@ -14,8 +14,34 @@ class BooksTableViewCell: UITableViewCell {
     @IBOutlet weak var pagesLabel: UILabel!
     
     func setupWith(book: Book) {
+
+        let dateFormaterGet = DateFormatter()
+        dateFormaterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+
+        let date: Date? = dateFormaterGet.date(from: book.released)
+        guard let unwrappedDate = date else { return }
+        let formatedDate = dateFormatter.string(from: unwrappedDate)
+
+        let pagesFont = UIFont.italicSystemFont(ofSize: 12)
+        let pagesAttributes = [NSAttributedString.Key.font: pagesFont]
+        let pagesAttributedString = NSAttributedString(string: String(book.numberOfPages) + " pages", attributes: pagesAttributes)
+
+        let datesFont = UIFont.systemFont(ofSize: 14)
+        let datesAttributes = [NSAttributedString.Key.font: datesFont]
+        let datesAttributedString = NSAttributedString(string: "\(formatedDate)", attributes: datesAttributes)
+
         titleLabel.text = book.name
-        dateLabel.text = book.released
-        pagesLabel.text =  String(book.numberOfPages)
+        dateLabel.attributedText = datesAttributedString
+        pagesLabel.attributedText = pagesAttributedString
+    }
+}
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
     }
 }
